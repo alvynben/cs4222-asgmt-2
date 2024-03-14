759fd72383e03269d5016469ba5cf64914d5f1e6
alvynben <alvynben@gmail.com>
2024-03-15 01:47:57 +0800

    feat: update task 2 to use imu

--- a2p2.c
+++ a2p2.c
@@ -51,12 +51,31 @@ enum State
 static enum State state = _IDLE;
 static int counter_buzz_wait;
 static int counter_rtimer;
+static int prev_light_reading = 0;
+static bool light_time = true;
+static bool debounce = false;
 static struct rtimer timer_rtimer;
 static rtimer_clock_t timeout_rtimer = RTIMER_SECOND / 4;
 static rtimer_clock_t two_second_rtimer = RTIMER_SECOND * 2;
+#define IMU_THRESHOLD 150
+#define LIGHT_DELTA_THRESHOLD 30000
 /*---------------------------------------------------------------------------*/
 static void init_opt_reading(void);
 static void get_light_reading(void);
+static void init_mpu_reading(void);
+static void get_mpu_reading(void);
+
+static void
+print_mpu_reading(int reading)
+{
+  if (reading < 0)
+  {
+    printf("-");
+    reading = -reading;
+  }
+
+  printf("%d.%02d", reading / 100, reading % 100);
+}
 
 /*---------------------------------------------------------------------------*/
 void do_rtimer_timeout(struct rtimer *timer, void *ptr)
@@ -72,9 +91,13 @@ void do_rtimer_timeout(struct rtimer *timer, void *ptr)
 
   counter_rtimer++;
   printf(": %d (cnt) %d (ticks) %d.%d%d%d (sec) \n", counter_rtimer, now, s, ms1, ms2, ms3);
-  if (state == _IDLE) {
-    get_light_reading();
-  } else {
+  if (state == _IDLE)
+  {
+    light_time ? get_light_reading() : get_mpu_reading();
+    light_time = !light_time;
+  }
+  else
+  {
     init_opt_reading();
   }
 }
@@ -88,37 +111,25 @@ get_light_reading()
   if (value != CC26XX_SENSOR_READING_ERROR)
   {
     printf("OPT: Light=%d.%02d lux\n", value / 100, value % 100);
-    if (value > 1300) {
-      state = _BUZZ;
-      counter_buzz_wait = 0;
+    if (value - prev_light_reading > LIGHT_DELTA_THRESHOLD || prev_light_reading - value > LIGHT_DELTA_THRESHOLD)
+    {
+      if (!debounce)
+      {
+        state = _BUZZ;
+        counter_buzz_wait = 0;
+      }
     }
+    if (debounce)
+    {
+      debounce = false;
+    }
+    prev_light_reading = value;
   }
   else
   {
     printf("OPT: Light Sensor's Warming Up\n\n");
   }
   init_opt_reading();
-  
-}
-
-static void
-stop_buzzer()
-{
-  // printf("STOP BUZZER\n");
-  buzzer_stop();
-  state = _BUZZ;
-  counter_buzz_wait++;
-  if (counter_buzz_wait == 4) {
-    state = _IDLE;
-  }
-}
-
-static void
-start_buzzer()
-{
-  // printf("START BUZZER\n");
-  buzzer_start(2093);
-  state = _WAIT;
 }
 
 static void
@@ -127,11 +138,70 @@ init_opt_reading(void)
   SENSORS_ACTIVATE(opt_3001_sensor);
 }
 
+static void
+stop_buzzer()
+{
+  buzzer_stop();
+  state = _BUZZ;
+  counter_buzz_wait++;
+  if (counter_buzz_wait == 4)
+  {
+    state = _IDLE;
+    debounce = true;
+  }
+}
+// MPU Sensor
+
+static void
+get_mpu_reading()
+{
+  int value;
+
+  value = mpu_9250_sensor.value(MPU_9250_SENSOR_TYPE_ACC_X);
+  if (value < -IMU_THRESHOLD || value > IMU_THRESHOLD)
+  {
+    state = _BUZZ;
+    counter_buzz_wait = 0;
+  }
+  printf("MPU Acc: X=%d", value);
+  printf("value < IMU_THRESHOLD: %d", value < IMU_THRESHOLD);
+  printf("State: %d\n", state);
+
+  value = mpu_9250_sensor.value(MPU_9250_SENSOR_TYPE_ACC_Y);
+  if (value < -IMU_THRESHOLD || value > IMU_THRESHOLD)
+  {
+    state = _BUZZ;
+    counter_buzz_wait = 0;
+  }
+  printf("MPU Acc: Y=%d", value);
+  printf("value < IMU_THRESHOLD: %d", value < IMU_THRESHOLD);
+  printf("State: %d\n", state);
+
+  value = mpu_9250_sensor.value(MPU_9250_SENSOR_TYPE_ACC_Z);
+  if (value < -IMU_THRESHOLD || value > IMU_THRESHOLD)
+  {
+    state = _BUZZ;
+    counter_buzz_wait = 0;
+  }
+  printf("MPU Acc: Z=%d", value);
+  printf("value < IMU_THRESHOLD: %d", value < IMU_THRESHOLD);
+  printf("State: %d\n", state);
+
+  init_mpu_reading();
+}
+
+static void
+init_mpu_reading(void)
+{
+  mpu_9250_sensor.configure(SENSORS_ACTIVE, MPU_9250_SENSOR_TYPE_ALL);
+}
+
 PROCESS_THREAD(process_rtimer, ev, data)
 {
   PROCESS_BEGIN();
   init_opt_reading();
   buzzer_init();
+  init_mpu_reading();
 
   printf(" The value of RTIMER_SECOND is %d \n", RTIMER_SECOND);
   printf(" The value of timeout_rtimer is %d \n", timeout_rtimer);
@@ -149,7 +219,8 @@ PROCESS_THREAD(process_rtimer, ev, data)
     }
     else if (state == _BUZZ)
     {
-      start_buzzer();
+      buzzer_start(2093);
+      state = _WAIT;
       rtimer_set(&timer_rtimer, RTIMER_NOW() + two_second_rtimer, 0, do_rtimer_timeout, NULL);
     }
     PROCESS_YIELD();
